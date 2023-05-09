4028d656416fd6957be59ced19bd0e06a299b7aa
Jensone <hello@jensone.com>
2023-05-09 11:35:01 +0200

    ✨ Page world.php

---
+++ world.php
@@ -0,0 +1,77 @@
+<?php include 'partials/_head.html.php'; ?>
+
+
+
+
+
+
+
+
+<style>
+    .continent-subtitle {
+        background: rgb(0,0,0);
+        background: linear-gradient(0deg, rgba(0,0,0,0) 0%, rgba(0,0,0,0) 48%, rgba(0,0,0,1) 49%, rgba(0,0,0,1) 51%, rgba(0,0,0,0) 52%, rgba(0,0,0,0) 100%);
+
+    }
+    .continent-subtitle h3 {
+        background: #ffffff;
+        border-radius: 5rem;
+        width: fit-content;
+        padding: 0.5rem 1rem;
+    }
+</style>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<h1>Tous les pays du monde</h1>
+<div class="continent-subtitle">
+    <h3>Europe</h3>
+</div>
+
+<ul>
+    <li>
+        <a href="">
+            <img src="" alt="">
+            France
+        </a>
+    </li>
+    <li>
+        <a href="">
+            <img src="" alt="">
+            Espagne
+        </a>
+    </li>
+    <li>
+        <a href="">
+            <img src="" alt="">
+            Portugal
+        </a>
+    </li>
+    <li>
+        <a href="">
+            <img src="" alt="">
+            Royaume-Uni
+        </a>
+    </li>
+</ul>
+
+<?php include 'partials/_footer.html.php'; ?>
